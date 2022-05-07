8fd0bfe4bb2db1756bf3a7ac48e0f763b6898040
ZorEl212 <93014559+ZorEl212@users.noreply.github.com>
2022-05-07 15:25:51 +0000

    Update nad_j4primelte.mk

--- nad_j4primelte.mk
+++ nad_j4primelte.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/nad/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Inherit MIUI Camera
 # $(call inherit-product, vendor/MiuiCamera/config.mk)
